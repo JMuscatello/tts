9c063321ebe792ef877a7e623e0b0e0effd51995
JMuscatello <jordan.muscatello@gmail.com>
2022-12-14 17:26:51 +0000

    Add transcription preprocessing

--- yt_rip/utils/download.py
+++ yt_rip/utils/download.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 from yt_dlp import YoutubeDL
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -27,3 +27,52 @@ def get_video_ids_from_playlist(playlist_url: str, only_captions: bool = False) 
             video_ids.append(entry['id'])
 
     return video_ids
+
+def preprocess_transcriptions(transcriptions: List[Dict]) -> List[Dict]:
+    """Preprocesses list of transcriptions.
+    Removes items containing music. Combines transcriptions if less that 1s between them
+
+    Args:
+        transcriptions (List[Dict]): List of dictionaries with 'text', 'stat' and 'duration' fields
+
+    Returns:
+        List of refined transcription dictionarires in same format as input data
+    """
+
+    new_transcriptions = []
+    previous_item = None
+
+    def validate_item(item):
+        if '[Music]' in item['text']:
+            return False
+        if len(item['text'].split()) <= 1:
+            return False
+        if item['duration'] < 1.0:
+            return False
+        return True
+
+    for item in transcriptions:
+        if not validate_item(item):
+            print(item['text'])
+            if previous_item:
+                new_transcriptions.append(previous_item)
+                previous_item = None
+            continue
+        if previous_item:
+            # check if current transcription begins > 1s from end of previous
+            previous_end = previous_item['start'] + previous_item['duration']
+            if item['start'] - previous_end < 0.2:
+                previous_item['text'] = previous_item['text'] + ' ' + item['text']
+                previous_item['duration'] = (
+                    item['start'] - previous_item['start'] + item['duration'])
+            else:
+                new_transcriptions.append(previous_item)
+                previous_item = item
+
+        if not previous_item:
+            previous_item = item
+
+    if previous_item and validate_item(previous_item):
+        new_transcriptions.append(previous_item)
+
+    return new_transcriptions
